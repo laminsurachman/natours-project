0b5dbae25d4156867e5a9ae0b271a3fb0c7c82c8
laminsurachman <rahman0669@gmail.com>
2024-07-27 15:46:31 +0700

    menambah i mongoose dan conect ke mongodb

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-console */
+/* eslint-disable import/extensions */
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app.js');
 
 // console.log(process.env);
+
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
