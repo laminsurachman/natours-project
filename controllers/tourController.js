f083f4de3be309086894e79cd44a6c582939aefa
laminsurachman <rahman0669@gmail.com>
2024-07-28 12:20:17 +0700

    membuat sorting,limit dan pagination filds dan route nya

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-const-assign */
 /* eslint-disable no-unused-vars */
 /* eslint-disable node/no-unsupported-features/es-syntax */
 /* eslint-disable import/no-useless-path-segments */
@@ -9,22 +11,62 @@
 // eslint-disable-next-line no-unused-vars
 const Tour = require('./../models/tourModel');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
 exports.getAllTours = async (req, res) => {
   try {
+    console.log(req.query);
+    //Build Query
+    // 1.a Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    console.log(req.query, queryObj);
+    //1.badvace filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    const query = Tour.find(queryObj);
-    // .where('durations')
-    // .equal(5)
-    // .where('difficulty')
-    // .equals('easy');
+    let query = Tour.find(JSON.parse(queryStr));
+
+    //2. Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    //3. Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    //4. Pagination
+
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    //page 3, limit 10, page1 1-10, page2 11-20, page3 21-30
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
+    //Executing Query
 
     const tours = await query;
 
+    //send response
     res.status(200).json({
       status: 'success',
       requestedAt: req.requestTime,
